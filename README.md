d6df74b88937db5bead22f3100c6961a9249c09a
Soufien Kallel <soufien.kallel@example.com>
2026-02-16 10:59:57 +0100

    backend

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # auto-diagnostic-platform
 Platform for remote and intelligent auto diagnostic
+Location: C:\auto diagnostic platform\backend\.venv\Lib\site-packages
---
+++ backend/app/schemas/user.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel, EmailStr
+
+
+class UserRegister(BaseModel):
+    
+    first_name: str
+    last_name: str
+    email: EmailStr
+    phone: str
+    password: str
+
+
+class UserLogin(BaseModel):
+    """Schema pour se connecter"""
+    email: EmailStr
+    password: str
+
+
+class UserResponse(BaseModel):
+    """Schema pour la réponse utilisateur"""
+    user_id: int
+    email: str
+    
