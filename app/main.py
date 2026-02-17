f4b7e38fe452f2ed9035489215358f1303d09141
Soufien Kallel <soufien.kallel@example.com>
2026-02-17 14:48:31 +0100

    management alert

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 
-from app.api.v1 import auth, database, fleet, vehicle
+from app.api.v1 import alert, auth, database, fleet, vehicle
 # Import les modèles pour enregistrer les tables
+from app.models import alert as alert_model
 from app.models import fleet as fleet_model
 from app.models import user, vehicle as vehicle_model
 
@@ -17,6 +18,7 @@ app.include_router(database.router, prefix="/api/v1", tags=["Database"])
 app.include_router(auth.router, prefix="/api/v1", tags=["Authentication"])
 app.include_router(fleet.router, prefix="/api/v1", tags=["Fleets"])
 app.include_router(vehicle.router, prefix="/api/v1", tags=["Vehicles"])
+app.include_router(alert.router, prefix="/api/v1", tags=["Alerts"])
 
 
 @app.get("/")
